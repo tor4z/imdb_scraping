2af4a7788871bd7cc87623af62681c5fa3a03464
tor4z <vwenjie@hotmail.com>
2018-04-21 00:11:47 +0800

    fix entry use new api

--- imdb/main.py
+++ imdb/main.py
@@ -2,17 +2,11 @@ from crabs import Client, HTMLParser, StrParser, Set
 import re
 
 if __name__ == "__main__":
-    url = "http://www.imdb.com"
+    domain = "http://www.imdb.com"
     client = Client()
-    client.get(url)
-    parser = HTMLParser(client.text, "lxml")
-    links = parser.find_all("a", href = re.compile(r"[.*title.*|.*name.*]"))
-    title_parser = StrParser(r".*title/(\w*)/?")
-    name_parser = StrParser(r".*name/(\w*)/?")
-    title_set = Set()
-    name_set = Set()
+    client.get(domain)
+    parser = HTMLParser(client.page, "lxml")
+    links = parser.find_all_links()
 
     for link in links:
-        title_set.add(title_parser.find_one(link))
-        name_set.add(name_parser.find_one(link))
-    print(title_set)
+        print(link)
