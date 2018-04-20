43a50bfb2f8da6cd7c046553de19b73e6bb2b149
tor4z <vwenjie@hotmail.com>
2018-04-21 00:10:38 +0800

    remove urls

--- imdb/urls.py
+++
@@ -1,9 +0,0 @@
-def title_to_url(title_id):
-    if title_id is None:
-        raise TypeError
-    return "https://www.imdb.com/title/{0}/".format(title_id)
-
-def name_to_url(name_id):
-    if name_id is None:
-        raise TypeError
-    return "https://www.imdb.com/name/{0}/".format(name_id)
