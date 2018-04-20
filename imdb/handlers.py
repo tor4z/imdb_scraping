c6b233430b0f70de3234551d848e13c11717d7c1
tor4z <vwenjie@hotmail.com>
2018-04-21 02:32:00 +0800

    Use new handler api

--- imdb/handlers.py
+++ imdb/handlers.py
@@ -1,20 +1,13 @@
 from crabs import Handler
+from crabs import HTMLParser
 
 class NameHandler(Handler):
-    _PATTERN = r".*name/(\w*)/?"
+    pass
 
 
 class TitleHandler(Handler):
-    _PATTERN = r".*title/(\w*)/?"
-
     def init(self):
         pass
 
-    def new_url(self):
+    def get(self):
         pass
-
-    def put_link(self, links):
-        if not isinstance(links, list):
-            links = [links]
-        self._crabs.put_links(links)
-
