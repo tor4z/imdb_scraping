9e4e0d79388f6789b181dcb03fab71d75fded731
tor4z <vwenjie@hotmail.com>
2018-04-21 20:35:12 +0800

    set client headers

--- imdb/main.py
+++ imdb/main.py
@@ -1,4 +1,5 @@
 from crabs import Crabs
+from crabs.utils import ClientHeaders
 from imdb.routes import routes
 
 if __name__ == "__main__":
@@ -7,5 +8,6 @@ if __name__ == "__main__":
     crabs.set_max_depth(3)
     crabs.set_seeds(seed)
     crabs.set_routes(routes)
+    crabs.set_client_headers(ClientHeaders)
     crabs.set_allow_netloc(["www.imdb.com"])
     crabs.run()
