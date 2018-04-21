0dfb117913acd1754dc0d6faa4b891a6c55af0db
tor4z <vwenjie@hotmail.com>
2018-04-21 19:10:04 +0800

    Use crabs new api

--- imdb/main.py
+++ imdb/main.py
@@ -4,6 +4,8 @@ from imdb.routes import routes
 if __name__ == "__main__":
     seed = ["http://www.imdb.com"]
     crabs = Crabs()
+    crabs.set_max_depth(3)
     crabs.set_seeds(seed)
     crabs.set_routes(routes)
+    crabs.set_allow_netloc(["www.imdb.com"])
     crabs.run()
