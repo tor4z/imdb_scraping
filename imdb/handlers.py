2c2447666efe882f2cd24885327a0d273a460fd0
tor4z <vwenjie@hotmail.com>
2018-04-24 04:32:46 +0800

    Use crabs new api

--- imdb/handlers.py
+++ imdb/handlers.py
@@ -2,10 +2,10 @@ from crabs import Handler
 from crabs import HTMLParser
 
 class NameHandler(Handler):
-    def get(self):
+    def get(self, resp):
         print("get name")
 
 
 class TitleHandler(Handler):
-    def get(self):
+    def get(self, resp):
         print("get title")
--- imdb/main.py
+++ imdb/main.py
@@ -1,13 +1,15 @@
 from crabs import Crabs
-from crabs.utils import ClientHeaders
 from imdb.routes import routes
+import logging
 
 if __name__ == "__main__":
     seed = ["http://www.imdb.com"]
     crabs = Crabs()
     crabs.set_max_depth(3)
     crabs.set_seeds(seed)
-    crabs.set_routes(routes)
-    crabs.set_client_headers(ClientHeaders)
+    crabs.set_log_level(logging.WARNING)
+    crabs.set_client_max_redirects(5)
+    crabs.update_routes(routes)
     crabs.set_allow_netloc(["www.imdb.com"])
+    crabs.set_disallow_path(["/offsite/*"])
     crabs.run()
