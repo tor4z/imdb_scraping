9945507d7577fcb428ca607bb59bb0491fa429b0
tor4z <vwenjie@hotmail.com>
2018-04-25 01:16:33 +0800

    Update seed 163

--- imdb/main.py
+++ imdb/main.py
@@ -3,13 +3,14 @@ from imdb.routes import routes
 import logging
 
 if __name__ == "__main__":
-    seed = ["http://www.acfun.cn"]
+    seed = ["http://www.163.com"]
     crabs = Crabs()
     crabs.set_max_depth(2)
     crabs.set_seeds(seed)
-    crabs.set_log_level(logging.INFO)
+    crabs.set_logger_level(logging.INFO)
     crabs.set_client_max_redirects(5)
     crabs.update_routes(routes)
-    crabs.set_allow_netloc(["www.acfun.cn"])
-    crabs.set_disallow_path(["/reg/*", "/login/*", "/info/*"])
+    crabs.set_allow_netloc(["*.163.com"])
+    crabs.set_disallow_path([])
+    crabs.set_executor(10)
     crabs.run()
