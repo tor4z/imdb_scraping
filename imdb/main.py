48a9e1aa86b47a82acae41e7ba052c2d58cc777c
tor4z <vwenjie@hotmail.com>
2018-04-24 15:07:08 +0800

    Update domain

--- imdb/main.py
+++ imdb/main.py
@@ -3,13 +3,13 @@ from imdb.routes import routes
 import logging
 
 if __name__ == "__main__":
-    seed = ["http://www.imdb.com"]
+    seed = ["http://www.acfun.cn"]
     crabs = Crabs()
     crabs.set_max_depth(3)
     crabs.set_seeds(seed)
     crabs.set_log_level(logging.WARNING)
     crabs.set_client_max_redirects(5)
     crabs.update_routes(routes)
-    crabs.set_allow_netloc(["www.imdb.com"])
-    crabs.set_disallow_path(["/offsite/*"])
+    crabs.set_allow_netloc(["www.acfun.cn"])
+    crabs.set_disallow_path(["/reg/*", "/login/*", "/info/*"])
     crabs.run()
