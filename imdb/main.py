18e32914209c7c8ef42e92d4c439bae0ad848a57
tor4z <vwenjie@hotmail.com>
2018-04-25 03:29:40 +0800

    Use new api

--- imdb/main.py
+++ imdb/main.py
@@ -1,16 +1,19 @@
 from crabs import Crabs
+from crabs.options import Travel
 from imdb.routes import routes
 import logging
 
 if __name__ == "__main__":
     seed = ["http://www.163.com"]
     crabs = Crabs()
-    crabs.set_max_depth(2)
     crabs.set_seeds(seed)
-    crabs.set_logger_level(logging.INFO)
-    crabs.set_client_max_redirects(5)
-    crabs.update_routes(routes)
-    crabs.set_allow_netloc(["*.163.com"])
-    crabs.set_disallow_path([])
+    crabs.set_logger(level=logging.DEBUG)
+    crabs.set_http_client(max_redirects=5)
+    crabs.set_routes(routes)
+
+    crabs.set_url_filter(allow_netlocs = ["*.163.com"],
+                        disallow_paths = [])
+    crabs.set_scraper(depth = 2, travel_mod = Travel.BFS)
+
     crabs.set_executor(10)
     crabs.run()
